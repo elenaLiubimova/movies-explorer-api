e6a18d4b680022e3b6a1a2e50cf33a644125cf8e
Elena Liubimova <elena.liubimova3@gmail.com>
2023-05-21 01:31:36 +0300

    feat: добавить обработку cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes');
 const { handleErrors } = require('./middlewares/handleErrors');
 const limiter = require('./rateLimiter');
@@ -16,6 +17,7 @@ const dbUrl = NODE_ENV === 'production' ? DB_PRODUCTION : DB;
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(cors());
 mongoose.connect(dbUrl);
 app.use(helmet());
 app.use(express.json());
